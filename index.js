6a3514144bb26e6d339fe59c0123c2d51f7906ea
p-wtt <parksangmin.wyatt@gmail.com>
2021-06-09 17:25:59 +0900

    nomadcoders typescript video

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sayHi = (name, age, gender) => {
+    return `Hellog ${name}, you ar ${age}, you are a ${gender}`;
+};
+console.log(sayHi("wyatt", 27, "male"));
+//# sourceMappingURL=index.js.map
